8393601e1c9edb9dfecce6629c50a483130553f7
charlyiiv <charlyiiv@hotmail.com>
2026-07-15 13:17:53 +0000

    Install Google Analytics correctly

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
+import { GoogleAnalytics } from "@next/third-parties/google";
 import "./globals.css";
-import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "GENOVEVA | Fairline Squadron 58 Share For Sale",
@@ -13,25 +13,13 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode;
-}) {
+}>) {
   return (
     <html lang="en">
-  <body>{children}</body>
-
-  <Script
-    src="https://www.googletagmanager.com/gtag/js?id=G-PTSK13E58M"
-    strategy="afterInteractive"
-  />
-
-  <Script id="google-analytics" strategy="afterInteractive">
-    {`
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-
-      gtag('config', 'G-PTSK13E58M');
-    `}
-  </Script>
-</html>
+      <body>{children}</body>
+      <GoogleAnalytics gaId="G-PTSK13E58M" />
+    </html>
+  );
+}
